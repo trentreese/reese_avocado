9e903402ed0cb3ef2b7cf3437475c0ef8b0a04d3
Trent Reese <trent24@gmail.com>
2019-07-16 22:38:19 +0200

    checkbox updates

--- home/books.py
+++ home/books.py
@@ -11,17 +11,18 @@ def get_currentlyreading(req_type):
 	headers = {
 	    'Content-type': 'application/json',
 	}
-	url_curl = 'https://www.goodreads.com/review/list/93087855.xml?key=' + client_key + '&v=2&shelf=' + req_type + '&per_page=200&page=1'
-	r = requests.get(url_curl, headers=headers)
-	e = ET.ElementTree(ET.fromstring(r.content))
 	reading = []
-	for elt in e.iter():
-		if elt.tag == "title":
-			title = elt.text
-		if elt.tag == "image_url":
-			image_url = elt.text
-		if elt.tag == "name":
-			author = elt.text
-			reading.append([title, author, image_url])
+	if req_type != "":
+		url_curl = 'https://www.goodreads.com/review/list/93087855.xml?key=' + client_key + '&v=2&shelf=' + req_type + '&per_page=200&page=1'
+		r = requests.get(url_curl, headers=headers)
+		e = ET.ElementTree(ET.fromstring(r.content))
+		for elt in e.iter():
+			if elt.tag == "title":
+				title = elt.text
+			if elt.tag == "image_url":
+				image_url = elt.text
+			if elt.tag == "name":
+				author = elt.text
+				reading.append([title, author, image_url])
 
 	return reading
--- home/views.py
+++ home/views.py
@@ -1,10 +1,23 @@
 from django.http import HttpResponse
 from django.template import loader
 from . import books
+from .forms import NameForm
 
 def index(request):
     template = loader.get_template('home/index.html')
-    book_list = books.get_currentlyreading("currently-reading")
+    book_type = ""
+    some_var = request.POST.getlist('checks')
+    print(some_var)
+    if some_var == "id_cr":
+        # Checkbox was selected
+        book_type = "currently-reading"
+    elif 'reading' in request.GET:
+        book_type = "read"
+    else:
+        book_type = ""
+        
+
+    book_list = books.get_currentlyreading(book_type)
     context = {
         'book_list': book_list
     }
