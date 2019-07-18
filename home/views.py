3b365f477a96ea52070f35ede75a4f03963067e9
Trent Reese <trent24@gmail.com>
2019-07-18 22:42:23 +0200

    updated radios

--- home/views.py
+++ home/views.py
@@ -1,21 +1,22 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
 from . import books
 from .forms import NameForm
 
+@csrf_exempt 
 def index(request):
     template = loader.get_template('home/index.html')
     book_type = ""
-    some_var = request.GET.getlist('checks')
-    print(some_var)
-    if some_var == "id_cr":
-        # Checkbox was selected
+    if request.POST.get('radios')== "id_cr":
+        print("cr")
         book_type = "currently-reading"
-    elif 'reading' in request.GET:
+    elif request.POST.get('radios') == "id_r":
+        print("r")
         book_type = "read"
     else:
-        book_type = "currently-reading"
-        
+        print("other")
+        book_type = "currently-reading" # default to currently-reading on page load
 
     book_list = books.get_currentlyreading(book_type)
     context = {
