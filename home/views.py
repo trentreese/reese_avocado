4296738b7f8370ba377a6a7830dda97790278896
trentreese <trent24@gmail.com>
2019-10-27 15:10:42 +0100

    added pagination

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
+from django.core.paginator import Paginator
 from . import books
 from .forms import form_r
 
@@ -9,7 +10,7 @@ from .forms import form_r
 def get_books(request):
     # if this is a POST request we need to process the form data
     book_type = ""
-    default_choice = "1"
+
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = form_r(request.POST)
@@ -18,21 +19,36 @@ def get_books(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            default_choice = form.cleaned_data['choice_field']
             if form.cleaned_data['choice_field'] == "1":
                 book_type = "currently-reading"
+                choice = "1"
             elif form.cleaned_data['choice_field'] == "2":
                 book_type = "read"
+                choice = "2"
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        book_type = "currently-reading"
+        choice = request.GET.get('choice')
+        if choice:
+            if str(choice) == "1":
+                book_type = "currently-reading"
+            else:
+                book_type = "read"
+        else:
+            print("Choice doesn't exist ")
+            choice = "1"
+            book_type = "currently-reading"
 
     template = loader.get_template('home/reading.html')
     book_list = books.get_currentlyreading(book_type)
-    form = form_r({'choice_field': default_choice})
+    paginator = Paginator(book_list, 3)
+    page = request.GET.get('page')
+    book_pages = paginator.get_page(page)
+
+    form = form_r({'choice_field': str(choice)})
     context = {
-        'book_list': book_list,
-        'form': form
+        'book_list': book_pages,
+        'form': form,
+        'choice': choice
     }
     return HttpResponse(template.render(context, request))
