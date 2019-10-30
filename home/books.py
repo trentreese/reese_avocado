b77d22a9b3749cb4de2de00eb3d513f8ee01b670
trentreese <trent24@gmail.com>
2019-10-30 22:52:52 +0100

    added functions and pagination on curl call

--- home/books.py
+++ home/books.py
@@ -7,17 +7,33 @@ def get_currentlyreading(req_type):
     client_key = good_reads_account.key
     headers = {'Content-type': 'application/json'}
     reading = []
+    reading_final = []
     if req_type != "":
-        url_curl = 'https://www.goodreads.com/review/list/93087855.xml?key=' + client_key + '&v=2&shelf=' + req_type + '&per_page=200&page=1'
-        r = requests.get(url_curl, headers=headers)
-        e = ET.ElementTree(ET.fromstring(r.content))
-        for elt in e.iter():
-            if elt.tag == "title":
-                title = elt.text
-            if elt.tag == "image_url":
-                image_url = elt.text
-            if elt.tag == "name":
-                author = elt.text
-                reading.append([title, author, image_url])
+        page = 1
+        while page > 0:
+            url_curl = 'https://www.goodreads.com/review/list/93087855.xml?key=' + client_key + '&v=2&shelf=' + req_type + '&per_page=10&page=' + str(page)
+            print(url_curl)
+            r = requests.get(url_curl, headers=headers)
+            reading = set_reading_list(r.content)
+            if reading:
+                reading_final += reading
+                page += 1
+            else:
+                page = 0
 
-    return reading
+    return reading_final
+
+def set_reading_list(content):
+
+    readinglist = []
+    e = ET.ElementTree(ET.fromstring(content))
+    for elt in e.iter():
+        if elt.tag == "title":
+            title = elt.text
+        if elt.tag == "image_url":
+            image_url = elt.text
+        if elt.tag == "name":
+            author = elt.text
+            readinglist.append([title, author, image_url])
+
+    return readinglist
--- home/views.py
+++ home/views.py
@@ -9,7 +9,6 @@ from .forms import form_r
 @csrf_exempt
 def get_books(request):
     # if this is a POST request we need to process the form data
-    book_type = ""
 
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -17,27 +16,11 @@ def get_books(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            if form.cleaned_data['choice_field'] == "1":
-                book_type = "currently-reading"
-                choice = "1"
-            elif form.cleaned_data['choice_field'] == "2":
-                book_type = "read"
-                choice = "2"
+            book_type, choice = set_list_type(form.cleaned_data['choice_field'])
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        choice = request.GET.get('choice')
-        if choice:
-            if str(choice) == "1":
-                book_type = "currently-reading"
-            else:
-                book_type = "read"
-        else:
-            print("Choice doesn't exist ")
-            choice = "1"
-            book_type = "currently-reading"
+        book_type, choice = set_list_type(request.GET.get('choice'))
 
     template = loader.get_template('home/reading.html')
     book_list = books.get_currentlyreading(book_type)
@@ -49,6 +32,19 @@ def get_books(request):
     context = {
         'book_list': book_pages,
         'form': form,
-        'choice': choice
+        'choice': str(choice)
     }
     return HttpResponse(template.render(context, request))
+
+
+def set_list_type(choice):
+    if choice:
+        if choice == "1":
+            book_type = "currently-reading"
+        elif choice == "2":
+            book_type = "read"
+    else:
+        choice = "1"
+        book_type = "currently-reading"
+
+    return book_type, choice
